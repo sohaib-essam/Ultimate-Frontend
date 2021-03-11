64afbf700a6087dafec2cdab620c2d2f0afdb3e4
sohaib-essam <afifimohammedessam@gmail.com>
2021-03-11 02:11:30 +0200

    Lesson 23, 24

--- stage/js/main.js
+++ stage/js/main.js
@@ -27,6 +27,21 @@ $(function () {
     $(this).find("i").toggleClass("fa-spin");
     $(this).parent().toggleClass("hide-settings");
   });
+
+  //Switch Colors Themes
+  let themesClasses = [];
+  $(".color-options li").each(function() {
+
+    themesClasses.push($(this).data("theme"))
+
+  })
+  $(".color-options li").on("click", function () {
+    $(this).addClass("active").siblings().removeClass("active")
+    $("body")
+    .removeClass(themesClasses.join(" "))
+    .addClass($(this).data("theme"));
+  })
+
 });
 
 var elem = document.documentElement;
